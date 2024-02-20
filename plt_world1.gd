6bd63c4ec27a19f8e5948ebee5589e9e19c2efeb
TimothySimth <143823817+TimothySimth@users.noreply.github.com>
2024-02-20 13:43:47 -0600

    2/20

--- plt_world1.gd
+++ plt_world1.gd
@@ -10,3 +10,19 @@ func _ready():
 func _on_interact_mouse_entered():
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		$interact.queue_free()
+
+
+var dragging = false
+var offset = Vector2.ZERO
+
+func _process(delta):
+	if dragging:
+		$draggable.global_position = get_global_mouse_position() - offset
+
+func _on_button_button_down():
+	dragging = true
+	offset = get_global_mouse_position() - $draggable.global_position
+
+func _on_button_button_up():
+	dragging = false
+	
