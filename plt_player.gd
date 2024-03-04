d9da19386a880df9c1d37c15771f0eb70e321d6e
TimothySimth <143823817+TimothySimth@users.noreply.github.com>
2024-03-04 13:43:07 -0600

    3/4

--- plt_player.gd
+++ plt_player.gd
@@ -9,18 +9,21 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
+	var direction = Input.get_axis("a", "d")# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		if is_on_wall():
+			velocity.y = JUMP_VELOCITY
+			velocity.x = direction * -10000
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("a", "d")
-	if direction:
+		
+	elif direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
